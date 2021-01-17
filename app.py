de18d623053e3ac4440da59c5763a38ab2417eb4
Alexandra Saizan <allie.saizan@gmail.com>
2021-01-17 17:04:50 -0500

    Adding modified app

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ import ppscore as pps
 import warnings
 warnings.filterwarnings("ignore")
 
-severity_historical = pd.read_csv("/Users/admin/Documents/cvi/severity-data/severity_historical.csv")
+severity_historical = pd.read_csv("https://raw.githubusercontent.com/community-insight-impact/longitudinal_metrics/main/severity_historical.csv")
 
+selectcols = severity_historical.columns.tolist()
+selectcols.remove("FIPS")
+selectcols.remove("year")
+
+st.set_page_config(layout="wide")
 st.title("Exploratory Analyses")
 st.subheader("View relationships within & between features over time")
 
@@ -17,23 +22,24 @@ d = st.sidebar.selectbox(label = "Select a year:",
                          index = 0)
 
 l = st.sidebar.selectbox(label = "Select a variable (for univariate distributions):",
-                        options = severity_historical.columns.tolist(),
+                        options = selectcols,
                         index = 0)
 
 subsetdf = severity_historical.loc[severity_historical["year"] == d].drop(labels = ["FIPS", "year"], axis = 1)
 
-
-col1, col2 = st.beta_columns(2)
+col1, col2, col3 = st.beta_columns(3)
 
 matrix_df = pps.matrix(subsetdf)[['x', 'y', 'ppscore']].pivot(columns='x', index='y', values='ppscore')
 
 fig1 = plt.figure(1)
-sns.heatmap(subsetdf.corr(), cmap="flare", annot=True).set_title("Correlation Matrix for {f}".format(f = d))
+sns.histplot(subsetdf, x = l).set_title("Univariate Distribution")
 
 fig2 = plt.figure(2)
-sns.heatmap(matrix_df, cmap="cubehelix_r", annot=True).set_title(("Predictive Power Score for {f}".format(f = d)))
+sns.heatmap(subsetdf.corr(), cmap="flare", annot=True).set_title("Correlation Matrix")
+
+fig3 = plt.figure(3)
+sns.heatmap(matrix_df, cmap="cubehelix_r", annot=True).set_title("Predictive Power Score")
 
 col1.pyplot(fig1, use_container_widths = True)
 col2.pyplot(fig2, use_container_widths = True)
-
-#st.pyplot(fig, use_container_width=True)
+col3.pyplot(fig3, use_container_widths = True)
